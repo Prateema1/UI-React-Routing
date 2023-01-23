675e77d416a87b056c4162c340a9573bee1059ee
Prateema1 <prateema.adhikari@gmail.com>
2023-01-23 16:04:50 +0530

    Added few basic react routes, new components and styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Header from "./components/Header";
+
+import "./styles/App.scss";
+import "./styles/Header.scss";
+
 function App() {
+  return (
+    <Router>
+    <Header />
+    <Routes>
+    <Route />
+    </Routes>
+    </Router>
+  )
 
 }
 
