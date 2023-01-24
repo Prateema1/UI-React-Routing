34aaa701fff23d621895b95d6de03f1101de6516
Prateema1 <prateema.adhikari@gmail.com>
2023-01-24 21:15:37 +0530

    Added new header & style components with static content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+    <div>
+    <h1>TechGiant</h1>
+    <p>@all rights reserved</p>
+    </div>
+
+    <div>
+    <h5>Follow Us</h5>
+    <div>
+    <a href="https://github.com/Prateema1" target={"blank"}>Github</a>
+    <a href="https://github.com/Prateema1" target={"blank"}>LinkedIn</a>
+    <a href="https://github.com/Prateema1" target={"blank"}>YouTube</a>
+    </div>
+    </div>
+    </footer>
+  )
+}
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,78 @@
 import React from "react";
-import vg from "../assests/2.webp";
+import vg from "../assests/react.png";
+import { AiFillGoogleCircle, 
+  AiFillAmazonCircle, 
+  AiFillYoutube, 
+  AiFillInstagram } from "react-icons/ai";
 
 const Home = () => {
   return (
     <>
-    <div className="home">
+    <div className="home" id="home">
      <main>
      <h1>TechGiant</h1>
-     <p>Solution to all your technical needs.</p>
+     <p>One Stop Platform For Your Tech Stack Knowledge.</p>
      </main>
     </div>
     <div className="home2">
     <img src={vg} alt="Graphics" />
     <div>
-    <p>This is a dummy text.This is a dummy text.
-    This is a dummy text.This is a dummy text.
-    This is a dummy text.This is a dummy text.
+    <p>Do not get confused with different technologies in the market.
+    Create projects and get hands on experiment with technologies.
     </p>
     </div>
+    </div>
+
+    <div className="home3" id="about">
     <div>
+    <h1>About Us</h1>
+    <p>With the rising amount of new tech stack in the market, 
+    it can be overwhelming to choose the correct technology for your programming needs. 
+    We have listed a basic roadmap for you. Your "One Stop Platform" to explore various technologics from 
+    Web development to Mobile development.
+    For instance, if you are interested in Web Development, Javascript will be the best fit for you.
+    By learning one language, you can quickly build a full stack web application. The most popular tech stack
+    based on JS is MEAN & MERN stack.
+    MEAN - (MongoDB, Express, Angular, Node)
+    MERN - (MongoDB, Express, React, Node)
+    We wil be adding more information soon...
+
+    </p>
+    </div>
+    </div>
+
+    <div className="home4" id="brands">
+    <div>
+    <h1>Companies</h1>
+    <article>
+    <div style={{
+      animationDelay: "0.3s"
+    }}>
+    <AiFillGoogleCircle />
+    <p>Google</p>
+    </div>
+
+    <div style={{
+      animationDelay: "0.5s"
+    }}>
+    <AiFillAmazonCircle />
+    <p>Amazon</p>
+    </div>
+
+    <div style={{
+      animationDelay: "0.7s"
+    }}>
+    <AiFillYoutube />
+    <p>Youtube</p>
+    </div>
+
+    <div style={{
+      animationDelay: "0.1s"
+    }}>
+    <AiFillInstagram />
+    <p>Instagram</p>
+    </div>
+    </article>
     </div>
     </div>
     </>
